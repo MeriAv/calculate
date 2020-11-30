312f33581dc054c9df93f0df3b4230da7deab6bc
MeriAv <mari.avdeeva1407@gmal.com>
2020-11-30 16:35:54 +0300

    Modified methods

--- src/main/java/StatsService.java
+++ src/main/java/StatsService.java
@@ -1,5 +1,6 @@
 public class StatsService {
 
+    StatsService servise = new StatsService();
 
     public int calculateSum(int[] months) {
         int sum = 0;
@@ -11,11 +12,9 @@ public class StatsService {
 
     public double calculateAverageSum(int[] months) {
         double average = 0;
-        int sum = 0;
-        for ( int month : months ) {
-            sum = month + sum;
-            average = sum / 12;
-        }
+
+        int sum = servise.calculateSum(months);
+        average = sum / 12;
         return average;
     }
 
@@ -41,12 +40,9 @@ public class StatsService {
 
 
     public int calculateAverageSumUnder(int[] months) {
-        int under = 0; // создаем переменную записывать в нее будем количество месяцев, где псумма была больше средней
+        int under = 0;
         int sum = 0;
-        for ( int month : months ) {
-            sum = month + sum;
-        } // считаем общую сумму
-        double average = sum / 12; // делим общую сумму на 12 что бы узнать среднюю сумму, получаем 15
+        double average = servise.calculateAverageSum(months);
         for ( int month : months ) {
             if (month < average) {
                 under = under + 1;
@@ -58,10 +54,7 @@ public class StatsService {
     public int calculateAverageSumOver(int[] months) {
         int over = 0;
         int sum = 0;
-        for ( int month : months ) {
-            sum = month + sum;
-        }
-        double average = sum / 12;
+        double average = servise.calculateAverageSum(months);
         for ( int month : months ) {
             if (month > average) {
                 over = over + 1;
